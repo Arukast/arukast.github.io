899c77725dbc139400fc9b32d0b0a24a1916ecb9
Arukast <altatubagus@gmail.com>
2026-07-16 11:04:29 +0700

    perf: fixed the render-blocking

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://arukast.github.io',
+  build: {
+    inlineStylesheets: 'always'
+  },
   vite: {
     plugins: [tailwindcss()],
   },
